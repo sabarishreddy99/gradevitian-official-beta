1b1ff23e66a2796a123040780fdcef6ebfe3dc92
sabarishreddy99 <64861603+sabarishreddy99@users.noreply.github.com>
2020-12-29 19:53:34 +0530

    Yes

--- CGPA Estimator.js
+++ CGPA Estimator.js
@@ -4,7 +4,7 @@
 function showPopup(){
 
   document.getElementById('pop-up-result').style.display="flex";
-  document.getElementById('pop-up-blur').style.filter="blur(20px)";
+  document.getElementById('pop-up-blur').style.filter="blur(30px)";
 }
 
 function closePopup(){
