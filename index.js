e09b211954f40c9d23c672e6e4164d006593d808
sabarishreddy99 <64861603+sabarishreddy99@users.noreply.github.com>
2020-12-29 17:15:43 +0530

    yeah

--- index.js
+++ index.js
@@ -68,3 +68,23 @@ window.onload = function() {
 
 
 //--------------------------------------- fixed-tops----------------------------------------
+
+ //Get the button of back to top
+//  var mybutton = document.getElementById("go-to-top");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function () { scrollFunction() };
+
+ function scrollFunction() {
+   if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    document.getElementById("go-to-top").style.display = "block";
+   } else {
+    document.getElementById("go-to-top").style.display = "none";
+   }
+ }
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
